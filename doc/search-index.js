170dc1ef2c6475e080fced64698062ad8a54a8e1
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-08-19 22:32:04 +0200

    Clean

--- doc/search-index.js
+++
@@ -1,5 +0,0 @@
-var searchIndex = JSON.parse('{\
-"lib":{"doc":"","t":"FF","n":["add","test"],"q":["lib",""],"d":["","Examples"],"i":[0,0],"f":[[[1,1],1],[[]]],"p":[[15,"usize"]]}\
-}');
-if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
-if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
