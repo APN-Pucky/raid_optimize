f73e0ac6c2f42a7fc42ddd25ee17b7878ed9e3e0
Alexander Puck Neuwirth <alexander@neuwirth-informatik.de>
2023-10-04 15:53:09 +0200

    mv 2

--- src2/data/class.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, PartialEq,strum_macros::Display, strum_macros::EnumIter, Deserialize, Serialize, Clone,Copy )]
-pub enum Class {
-    Attack,
-    Support,
-    Defense,
-}
--- src2/data/rarity.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, PartialEq,strum_macros::Display,strum_macros::EnumIter, Deserialize, Serialize, Clone,Copy )]
-pub enum Rarity {
-    Common, // White
-    Rare, // Green
-    Elite, // Blue
-    Epic, // Purple
-    Legendary, // Gold
-}
